783037d8ed59d86f12dcac0cf3aa4e8c6db6b788
MadelinMunoz <titaniamade@gmail.com>
2025-06-01 19:30:27 -0500

    conectado registrar a DB

--- conexionDB/conexion.php
+++ conexionDB/conexion.php
@@ -1,11 +1,7 @@
 <?php
-    $conexion=mysqli_connect("localhost", "root", "", "db_pastoeats");
-        if(!$conexion){
-            echo "Error de conexion: " . mysqli_connect_error();
-            exit();
-        }
-        else{
-            echo "Conectado a la base de datos";
-        }
+$conexion = mysqli_connect("localhost", "root", "", "db_pastoeats");
 
+if (!$conexion) {
+    die("❌ Error de conexión: " . mysqli_connect_error());
+}
 ?>
